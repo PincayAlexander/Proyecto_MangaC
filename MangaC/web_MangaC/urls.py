3b2b988136c1c30657bba46e9e0d90b498a9ec25
PincayAlexander <jpincay8421@utm.edu.ec>
2024-07-12 16:30:19 -0500

    Cambios 12/07/2024 Corregir error de doble login

--- MangaC/web_MangaC/urls.py
+++ MangaC/web_MangaC/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView
+from .views import CustomLoginView, CustomLogoutView
 from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('', views.home_view, name="home"),
     path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    path('accounts/logout/', views.logout_view, name="logout"),
+    path('accounts/logout/', CustomLogoutView.as_view(), name="logout"),
     path('signup/', views.signup_view, name="signup"),
     path('chapter/<str:mangaNombre>/', views.chapter_view, name="chapter"),
     path('about/', views.about_view, name="about"),
